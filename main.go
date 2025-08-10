0b22844e1e28ef2975322425afef27d23fb6338c
beacon776 <3214984261@qq.com>
2025-08-11 01:08:24 +0800

    update: 修改初始方法以及定时删除缓存

--- main.go
+++ main.go
@@ -7,7 +7,9 @@ import (
 	"logo_api/dao/mysql"
 	"logo_api/logger"
 	"logo_api/routes"
+	"logo_api/service"
 	"logo_api/settings"
+	"logo_api/util"
 	"net/http"
 	"os"
 	"os/signal"
@@ -33,12 +35,24 @@ func main() {
 		fmt.Printf("mysql.Init() failed, err: %s", err.Error())
 	}
 	zap.L().Debug("init mysql success...")
-	/*
-		// 4.初始化redis连接（如果有的话）
-		if err := redis.Init(settings.Config.RedisConfig); err != nil {
-			fmy
+	// 4.定时清除缓存
+	client, err := util.NewClient(settings.Config.CosConfig)
+	if err != nil {
+		zap.L().Fatal("util.NewClient", zap.Error(err))
+	}
+	resourceService := service.NewResourceService(client)
+
+	go func() {
+		ticker := time.NewTicker(10 * time.Minute)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case <-ticker.C:
+				resourceService.ClearExpiredCache(time.Hour)
+			}
 		}
-	*/
+	}()
 	// 5.注册路由
 	r := routes.Setup()
 	zap.L().Info("Starting HTTP server",
