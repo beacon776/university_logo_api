5310857a07288778d4e45f490cbb8dd6a48bf87d
beacon776 <3214984261@qq.com>
2025-08-11 00:56:44 +0800

    update: 修改插入方法并新增删除以及检查删除的方法

--- dao/mysql/mysql.go
+++ dao/mysql/mysql.go
@@ -8,12 +8,14 @@ import (
 	"github.com/jmoiron/sqlx"
 	"go.uber.org/zap"
 	"logo_api/settings"
+	"time"
 )
 
 var db *sqlx.DB
 
 func Init(config *settings.MysqlConfig) (err error) {
-	databaseSource := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True",
+	// 注意这里用 %%2F，因为 fmt.Sprintf 会把 % 作为格式符号，如果用单 %，会出错。
+	databaseSource := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Asia%%2FShanghai",
 		config.User,
 		config.Password,
 		config.Host,
@@ -50,7 +52,7 @@ func QueryFromNameAndSvg(preName string, ext string) (settings.UniversityResourc
 // QueryFromNameAndBitmap 在没有背景色的前提下，根据文件名、后缀（保证为位图）、边长/宽+高等参数查找对应的资源
 func QueryFromNameAndBitmap(preName string, ext string, size int, width int, height int) (settings.UniversityResources, error) {
 	var resource settings.UniversityResources
-	querySql := "SELECT * FROM university_resources WHERE (short_name = ? OR title = ?) AND resource_type = ? AND ((resolution_width = ? AND resolution_height = ?) OR (resolution_width = ? AND resolution_height = ?))"
+	querySql := "SELECT * FROM university_resources WHERE (short_name = ? OR title = ?) AND resource_type = ? AND ((resolution_width = ? AND resolution_height = ?) OR (resolution_width = ? AND resolution_height = ?)) AND is_deleted = 0 AND expire_time IS NOT NULL AND expire_time > NOW()"
 	err := db.Get(&resource, querySql, preName, preName, ext, size, size, width, height)
 	// 第一次查询出错
 	if err != nil {
@@ -58,7 +60,7 @@ func QueryFromNameAndBitmap(preName string, ext string, size int, width int, hei
 		if errors.Is(err, sql.ErrNoRows) {
 			zap.L().Error("current resource not found, next will try to find svg resource")
 			// 虽然直接查没查到，但是还有机会查到 svg 资源，继续去查 svg 资源
-			querySql = "SELECT * FROM university_resources WHERE (short_name = ? OR title = ?) AND resource_type = 'svg'"
+			querySql = "SELECT * FROM university_resources WHERE (short_name = ? OR title = ?) AND used_for_edge = 1"
 			err = db.Get(&resource, querySql, preName, preName)
 			// 第二次查询出错
 			if err != nil {
@@ -87,17 +89,33 @@ func InsertUniversityResource(universityResource settings.UniversityResources) e
 	sqlStr := `
 		INSERT INTO university_resources (
 			short_name, title, resource_name, resource_type, resource_md5,
-			resource_size_b, last_update_time, is_vector, is_bitmap,
-			resolution_width, used_for_edge, is_deleted, resolution_height
+			resource_size_b, last_update_time, upload_time, expire_time, is_vector, is_bitmap,
+			resolution_width, used_for_edge, is_deleted, resolution_height, background_color
 		) VALUES (
 			:short_name, :title, :resource_name, :resource_type, :resource_md5,
-			:resource_size_b, :last_update_time, :is_vector, :is_bitmap,
-			:resolution_width, :used_for_edge, :is_deleted, :resolution_height
+			:resource_size_b, :last_update_time, :upload_time, :expire_time, :is_vector, :is_bitmap,
+			:resolution_width, :used_for_edge, :is_deleted, :resolution_height, :background_color
 		)
 	`
 	_, err := db.NamedExec(sqlStr, universityResource)
 	if err != nil {
 		zap.L().Error("insert university_resource failed", zap.Error(err))
 	}
+	return err
+}
+
+// QueryExpiredResources 查询过期资源列表（不涉及 cos）
+func QueryExpiredResources(expireDuration time.Duration) ([]settings.UniversityResources, error) {
+	now := time.Now()
+	var expiredResources []settings.UniversityResources
+	err := db.Select(&expiredResources, `
+		SELECT * FROM university_resources
+		WHERE is_deleted = 0 AND upload_time <= ?`, now.Add(-expireDuration))
+	return expiredResources, err
+}
+
+// MarkResourceDeleted 删除过期资源
+func MarkResourceDeleted(id int) error {
+	_, err := db.Exec(`UPDATE university_resources SET is_deleted=1 WHERE id=?`, id)
 	return err
 }
