a062ecc8e2d49eeb0beee8e43250290541c1ef3b
beacon776 <3214984261@qq.com>
2025-09-08 16:14:54 +0800

    fix: 修改 Addr 拼接字段类型

--- dao/redis/redis.go
+++ dao/redis/redis.go
@@ -15,7 +15,7 @@ var rdb *redis.Client
 
 func Init(config *settings.RedisConfig) (err error) {
 	rdb = redis.NewClient(&redis.Options{
-		Addr:     fmt.Sprintf("%s:%s", config.Host, config.Port),
+		Addr:     fmt.Sprintf("%s:%d", config.Host, config.Port),
 		Password: config.Password,
 		DB:       config.DB,
 		PoolSize: config.PoolSize,
